2ab7ac37845d9a6a3ae1d2f3356417a83b543627
InkVak0485Prog <ISCat Admin@ISC-IFoxa>
2022-07-23 02:28:09 +0300

    обнова кода урона

--- src/main/java/net/mcreator/bvv/procedures/KillProcedure.java
+++ src/main/java/net/mcreator/bvv/procedures/KillProcedure.java
@@ -3,6 +3,7 @@ package net.mcreator.bvv.procedures;
 import net.minecraft.util.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
 
 import net.mcreator.bvv.BvvMod;
@@ -25,15 +26,19 @@ public class KillProcedure {
 		}
 		Entity entity = (Entity) dependencies.get("entity");
 		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
-		if (30 < ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).experienceLevel : 0)) {
+		double xp = 0;
+		xp = ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).experienceLevel : 0);
+		while (5 <= xp) {
+			if (entity instanceof LivingEntity) {
+				((LivingEntity) entity).attackEntityFrom(new DamageSource("\u043C\u0430\u0433\u0438\u0438").setDamageBypassesArmor(), (float) 5);
+			}
 			{
 				ItemStack _ist = itemstack;
-				if (_ist.attemptDamageItem((int) 50, new Random(), null)) {
+				if (_ist.attemptDamageItem((int) (5 * 1.25), new Random(), null)) {
 					_ist.shrink(1);
 					_ist.setDamage(0);
 				}
 			}
-			entity.attackEntityFrom(DamageSource.LAVA, (float) 30);
 		}
 	}
 }
