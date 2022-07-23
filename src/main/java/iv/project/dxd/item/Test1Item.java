5324faac635593a217d77fcbda7c8a882c786a81
InkVak0485Prog <ISCat Admin@ISC-IFoxa>
2022-07-23 22:29:08 +0300

    обнова

--- src/main/java/iv/project/dxd/item/Test1Item.java
+++ src/main/java/iv/project/dxd/item/Test1Item.java
@@ -2,18 +2,16 @@
 package iv.project.dxd.item;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.World;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.block.Blocks;
 
 import java.util.stream.Stream;
 import java.util.Map;
@@ -24,19 +22,19 @@ import iv.project.dxd.procedures.KillProcedure;
 import iv.project.dxd.DxdProjectModElements;
 
 @DxdProjectModElements.ModElement.Tag
-public class Test2Item extends DxdProjectModElements.ModElement {
-	@ObjectHolder("dxd_project:test_2")
+public class Test1Item extends DxdProjectModElements.ModElement {
+	@ObjectHolder("dxd_project:test_1")
 	public static final Item block = null;
 
-	public Test2Item(DxdProjectModElements instance) {
-		super(instance, 8);
+	public Test1Item(DxdProjectModElements instance) {
+		super(instance, 9);
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new SwordItem(new IItemTier() {
 			public int getMaxUses() {
-				return 10000;
+				return 47235;
 			}
 
 			public float getEfficiency() {
@@ -44,7 +42,7 @@ public class Test2Item extends DxdProjectModElements.ModElement {
 			}
 
 			public float getAttackDamage() {
-				return 6f;
+				return 2f;
 			}
 
 			public int getHarvestLevel() {
@@ -56,7 +54,7 @@ public class Test2Item extends DxdProjectModElements.ModElement {
 			}
 
 			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack(Blocks.IRON_BLOCK));
+				return Ingredient.fromStacks(new ItemStack(Items.NETHERITE_INGOT));
 			}
 		}, 3, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
 			@Override
@@ -72,12 +70,6 @@ public class Test2Item extends DxdProjectModElements.ModElement {
 								.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 				return retval;
 			}
-
-			@Override
-			@OnlyIn(Dist.CLIENT)
-			public boolean hasEffect(ItemStack itemstack) {
-				return true;
-			}
-		}.setRegistryName("test_2"));
+		}.setRegistryName("test_1"));
 	}
 }
--- src/main/java/iv/project/dxd/procedures/KillProcedure.java
+++ src/main/java/iv/project/dxd/procedures/KillProcedure.java
@@ -1,11 +1,72 @@
 package iv.project.dxd.procedures;
 
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Random;
 import java.util.Map;
+
+import iv.project.dxd.DxdProjectMod;
 
 public class KillProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				DxdProjectMod.LOGGER.warn("Failed to load dependency entity for procedure Kill!");
+			return;
+		}
+		if (dependencies.get("itemstack") == null) {
+			if (!dependencies.containsKey("itemstack"))
+				DxdProjectMod.LOGGER.warn("Failed to load dependency itemstack for procedure Kill!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
 		double xp = 0;
-
+		DxdProjectMod.LOGGER.info(("\u043F\u0440\u043E\u0432\u0435\u0440\u043A\u0430 XP:"
+				+ ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).experienceLevel : 0)));
+		xp = ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).experienceLevel : 0);
+		if (xp > 29) {
+			DxdProjectMod.LOGGER.info("\u0430\u0442\u0430\u043A\u0430 50");
+			if (entity instanceof LivingEntity) {
+				((LivingEntity) entity).attackEntityFrom(new DamageSource("\u043C\u0430\u0433\u0438\u0438").setDamageBypassesArmor(), (float) 50);
+			}
+			{
+				ItemStack _ist = itemstack;
+				if (_ist.attemptDamageItem((int) (30 * 1.25), new Random(), null)) {
+					_ist.shrink(1);
+					_ist.setDamage(0);
+				}
+			}
+		} else if (xp > 14) {
+			DxdProjectMod.LOGGER.info("\u0410\u0442\u0430\u043A\u0430 25");
+			if (entity instanceof LivingEntity) {
+				((LivingEntity) entity).attackEntityFrom(new DamageSource("\u043C\u0430\u0433\u0438\u0438").setDamageBypassesArmor(), (float) 25);
+			}
+			{
+				ItemStack _ist = itemstack;
+				if (_ist.attemptDamageItem((int) (15 * 1.25), new Random(), null)) {
+					_ist.shrink(1);
+					_ist.setDamage(0);
+				}
+			}
+		} else if (xp > 4) {
+			DxdProjectMod.LOGGER.info("\u0410\u0442\u0430\u043A\u0430 5");
+			if (entity instanceof LivingEntity) {
+				((LivingEntity) entity).attackEntityFrom(new DamageSource("\u043C\u0430\u0433\u0438\u0438").setDamageBypassesArmor(), (float) 5);
+			}
+			{
+				ItemStack _ist = itemstack;
+				if (_ist.attemptDamageItem((int) (5 * 1.25), new Random(), null)) {
+					_ist.shrink(1);
+					_ist.setDamage(0);
+				}
+			}
+		}
+		DxdProjectMod.LOGGER.info("\u0417\u0430\u0432\u0435\u0440\u0448\u0435\u043D\u043E");
 	}
 }
