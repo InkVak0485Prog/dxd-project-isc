ec5aa92e70fa509669d27555fd83f7672bc9f85b
InkVak0485Prog <ISCat Admin@ISC-IFoxa>
2022-07-23 04:39:17 +0300

    кккккк

---
+++ src/main/java/iv/project/dxd/item/AakyamItem.java
@@ -0,0 +1,75 @@
+
+package iv.project.dxd.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap;
+
+import iv.project.dxd.procedures.KillProcedure;
+import iv.project.dxd.DxdProjectModElements;
+
+@DxdProjectModElements.ModElement.Tag
+public class AakyamItem extends DxdProjectModElements.ModElement {
+	@ObjectHolder("dxd_project:aakyam")
+	public static final Item block = null;
+
+	public AakyamItem(DxdProjectModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 12460;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 6f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(Items.NETHERITE_INGOT));
+			}
+		}, 3, 1f, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				World world = entity.world;
+
+				KillProcedure.executeProcedure(
+						Stream.of(new AbstractMap.SimpleEntry<>("entity", entity), new AbstractMap.SimpleEntry<>("itemstack", itemstack))
+								.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+				return retval;
+			}
+		}.setRegistryName("aakyam"));
+	}
+}
